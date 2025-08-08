b5f6f81d6ab52556ea4aace141091997527cd787
Volhakov <olkakovka812@gmail.com>
2025-08-08 13:26:32 +0200

    ended my project

--- script.js
+++ script.js
@@ -7,57 +7,61 @@ function generateRandomUser() {
             const user = data.results[0];
             currentUser = user;
             displayUser(user);
-
-            document.querySelectorAll('.menu').forEach(menu => {
-                menu.innerHTML = '';
-            });
         })
         .catch(error => console.error('Error fetching random user:', error));
 }
 
 function displayUser(user) {
     const userContainer = document.querySelector('.user-container');
-    userContainer.innerHTML = `
-        <h2>${user.name.first} ${user.name.last}</h2>
-        <img src="${user.picture.medium}" alt="User Picture">`;
+    userContainer.innerHTML = ''; 
+
+    const selectedFields = Array.from(document.querySelectorAll('.menu-toggle:checked'))
+        .map(input => input.dataset.menu);
+
+    selectedFields.forEach(field => {
+        let content = '';
+
+        switch (field) {
+            case 'first-name':
+                content = `First Name: ${user.name.first}`;
+                break;
+            case 'last-name':
+                content = `Last Name: ${user.name.last}`;
+                break;
+            case 'picture':
+                content = `<img src="${user.picture.medium}" alt="User Picture">`;
+                break;
+            case 'country':
+                content = `Country: ${user.location.country}`;
+                break;
+            case 'city':
+                content = `City: ${user.location.city}`;
+                break;
+            case 'street':
+                content = `Street: ${user.location.street.name} ${user.location.street.number}`;
+                break;
+            case 'e-mail':
+                content = `Email: ${user.email}`;
+                break;
+            case 'age':
+                content = `Age: ${user.dob.age}`;
+                break;
+            case 'phone':
+                content = `Phone: ${user.phone}`;
+                break;
+            default:
+                break;
+        }
+
+        const div = document.createElement('div');
+        div.className = field;
+        div.innerHTML = content;
+        userContainer.appendChild(div);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('gender').addEventListener('click', () => {
-        if (!currentUser) return;
-        const menu = document.querySelector('#gender + .menu');
-        if (menu.innerHTML) {
-             menu.innerHTML = ''; 
-        } else {
-            menu.innerHTML = `<li>${currentUser.gender}</li>`; 
-        }
-
-    });
-
-    document.getElementById('name').addEventListener('click', () => {
-        const menu = document.querySelector('#name + .menu');
-        if (menu.innerHTML) {
-            menu.innerHTML = '';
-        } else {
-            menu.innerHTML = `<li>${currentUser.name.first} ${currentUser.name.last}</li>`;
-        }
-    });
-
-    document.getElementById('country').addEventListener('click', () => {
-        const menu = document.querySelector('#country + .menu');
-        if (menu.innerHTML) {           
-            menu.innerHTML = '';
-        } else {
-            menu.innerHTML = `<li>${currentUser.location.country}</li>`;
-        }
-    }); 
-
-    document.getElementById('age').addEventListener('click', () => {
-        const menu = document.querySelector('#age + .menu');
-        if (menu.innerHTML) {
-            menu.innerHTML = '';
-        } else {
-            menu.innerHTML = `<li>${currentUser.dob.age}</li>`;
-        }
-    });
+    const generateButton = document.querySelector('.generate-button');
+    generateButton.addEventListener('click', generateRandomUser);
 });
+
